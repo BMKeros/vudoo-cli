1326f0e832824ed027a8ba96983fa9e6157ebc12
Israel Lugo <hostelixisrael@gmail.com>
2018-04-23 13:11:54 -0400

    [FIX] fixed error to execute vudoo-cli

--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,15 @@
 const path = require('path');
-const local_path = require('../lib/local-path');
+const download_repository = require('download-git-repo');
+const shell = require('shelljs');
 const inquirer = require('inquirer');
 const ora = require('ora');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const generator = require('../lib/generator');
+const local_path = require('../lib/local-path');
+
+const rm = rimraf.sync;
+const exists = fs.existsSync;
 
 const action_init = (module_name, options) => {
     const template = options.template;
@@ -12,11 +20,11 @@ const action_init = (module_name, options) => {
     inquirer.prompt([{
         type: 'confirm',
         message: dest_generate === '.'
-          ? 'Generate project in current directory?'
-          : 'Target directory exists. Continue?',
+            ? 'Generate project in current directory?'
+            : 'Target directory exists. Continue?',
         name: 'ok'
     }]).then(answers => {
-        if (answers.ok){
+        if (answers.ok) {
             download_template(repository, template)
                 .then(path_download => generate_template(module_name, path_download, dest_generate))
                 .catch(err => console.log('Failed to download ' + repository + ': ' + err.message.trim()))
@@ -26,13 +34,19 @@ const action_init = (module_name, options) => {
 
 const generateNameRepository = (name) => {
     const main_repo = 'BMKeros/vudoo-templates';
-    switch(name){
-        case 'simple': return `${main_repo}#simple`;
-        case 'odoo8': return `${main_repo}#odoo8`;
-        case 'odoo9': return `${main_repo}#odoo9`
-        case 'odoo10': return `${main_repo}#odoo10`;
-        case 'odoo11': return `${main_repo}#odoo11`;
-        default: return `${main_repo}#simple`;
+    switch (name) {
+        case 'simple':
+            return `${main_repo}#simple`;
+        case 'odoo8':
+            return `${main_repo}#odoo8`;
+        case 'odoo9':
+            return `${main_repo}#odoo9`
+        case 'odoo10':
+            return `${main_repo}#odoo10`;
+        case 'odoo11':
+            return `${main_repo}#odoo11`;
+        default:
+            return `${main_repo}#simple`;
     }
 };
 
@@ -42,20 +56,20 @@ const download_template = (repo, template) => {
     return new Promise((resolve, reject) => {
         const spinner = ora('Downloading template from vudoo-templates');
         spinner.start();
-        
-        if (exists(dest_download)){
+
+        if (exists(dest_download)) {
             rm(dest_download);
         }
-        else{
+        else {
             shell.mkdir('-p', dest_download);
         }
 
         download_repository(repo, dest_download, { clone: false }, err => {
-            spinner.stop()
-            if (err){
+            spinner.stop();
+            if (err) {
                 reject(err);
             }
-            else{
+            else {
                 resolve(dest_download);
             }
         });
